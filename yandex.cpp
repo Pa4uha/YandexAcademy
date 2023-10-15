9faef1cf5b6289055cad300204c03efe53eb7048
Dmitry Zinchenko <dmitry@explosion.ru>
2023-10-15 15:44:56 +0300

    Сделал правки

--- yandex.cpp
+++ yandex.cpp
@@ -5,8 +5,10 @@ enum class Color {
 	Blue,
 };
 
+// Злдесь неправильный параметр, надо заменить 
+
 struct Point {
-	double x = 0.0;
+	double x = 1.0;
 	double y = 0.0;
 	double z = 0.0;
 	Color color;
